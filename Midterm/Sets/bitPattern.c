5c434b81bc7a8310f2413f694b0a0b6de57a31ed
livingsneaky-art <71326659+livingsneaky-art@users.noreply.github.com>
2022-10-09 09:42:27 +0800

    bitPattern

---
+++ Midterm/Sets/bitPattern.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void display(char value);
+
+int main(){
+	unsigned char value = 255;
+	
+	display(value);
+	
+	return;
+}
+
+void display(char value){
+	int gap, bits = (sizeof(value) * 8)-1;
+	unsigned int mask;
+	
+	for(gap = 1, mask = 1 << bits; mask > 0; mask >>= 1, gap++){
+		printf("%d ",(value & mask) ? 1 : 0);
+		if(gap % 4 == 0){
+			printf(" ");
+		}
+	}
+}
